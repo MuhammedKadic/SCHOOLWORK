fa7a715c6e259749d76f62eb6675c36da0a11d47
Muhammed kadic <muhammed_kadic@insideranken.org>
2019-12-04 10:01:29 -0600

    ADDED JS_HOT_4 TO REPO

---
+++ HOT TESTS/JS_HOT4/home_page.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // create an array of the slide images
+  var image, imageCache = [];
+  $("#slides img").each(function() {
+    image = new Image();
+    image.src = $(this).attr("src");
+    image.title = $(this).attr("alt");
+    imageCache[imageCache.length] = image;
+  });
+
+  // Start slide show
+  var imageCounter = 0;
+  var nextImage, nextCaption;
+  var timer = setInterval(function() {
+      $("#slide").fadeOut(1000, function() {
+        imageCounter = (imageCounter + 1) % imageCache.length;
+        nextImage = imageCache[imageCounter];
+        nextCaption = nextImage.title;
+        $("#slide").attr("src", nextImage.src).fadeIn(1000);
+      });
+    },
+    2000);
+
+});
